dc600676fbe3759cfef5b70900541082cc24f09f
Dmitriy <38374223+Disim@users.noreply.github.com>
2023-11-04 04:35:39 +0500

    2

--- bot/menu/paths.py
+++ bot/menu/paths.py
@@ -2,3 +2,4 @@ class Paths:
     SUPER_ADMIN_MAIN_PAGE = 'sa_main_page'
     ADMIN_MAIN_PAGE = 'a_main_page'
     USER_MAIN_PAGE = 'u_main_page'
+    ADMIN_GENERATE_INVITE_CODE_PAGE='admin_generate_invite_code_page'
